c49a38bcc041962c3fe99051ce8624efac993438
Jean-Charles Drubay <jcdrubay@trobz.com>
2013-07-14 19:38:18 +0700

    Adding the the sales dashboard to demo_dashboard.

--- demo_dashboard/__openerp__.py
+++ demo_dashboard/__openerp__.py
@@ -18,12 +18,18 @@ Demo data for Trobz Dashboard module
         'data/widget_number.xml',
         'data/widget_list.xml',
         'data/widget_graph.xml',
-    
         'data/dashboard_unit_test_data.xml',
+        'data/dashboard.test-unit_test.csv',
+        #Sales Dashboard
+        'data/dashboard/sales/dashboard_board_data.xml',
+        'data/dashboard/sales/dashboard_widget_data.xml',
+        'data/dashboard/sales/board_to_widget_rel_data.xml',
+        'data/dashboard/sales/dashboard_metric_data.xml',
+        'data/dashboard/sales/dashboard_field_data.xml',
     
+        #Menus (must be created last)
         'menu/dashboard.xml',
     
-        'data/dashboard.test-unit_test.csv'
     ],
     
     'demo': [],
--- demo_dashboard/model/sale_order_line.py
+++ demo_dashboard/model/sale_order_line.py
@@ -7,9 +7,12 @@ class sale_order_line(osv.osv, metric_support):
     _inherit = 'sale.order.line'
     
     _metrics_sql = {
+        'total_sales_numeric': 'SELECT sum(price_unit * product_uom_qty)/1000000000 as total_sales_numeric FROM sale_order_line WHERE TRUE {generated}',
+        'number_customers': 'SELECT COUNT(DISTINCT partner_id) as number_customers FROM sale_order WHERE TRUE {generated}',
+        'average_sale_repetition': 'select avg(nb_repetition) as nb_repetition from (SELECT COUNT(id) as "nb_repetition" FROM sale_order group by partner_id) as repetition_per_customer WHERE TRUE {generated}',
         'graph_total_sales': {
            'query': """
-                SELECT {group_sql} AS "{group_ref}", sum(price_unit * product_uom_qty) as total_sales 
+                SELECT {group_sql} AS "{group_ref}", sum(price_unit * product_uom_qty)/1000000000 as total_sales_amount
                 FROM sale_order_line sol
                 JOIN sale_order sor ON sor.id = sol.order_id
                 JOIN res_company rco ON rco.id=sor.company_id
@@ -17,7 +20,7 @@ class sale_order_line(osv.osv, metric_support):
                 JOIN res_partner rpa ON rpa.id=sor.partner_id
                 JOIN resource_resource rre ON rre.user_id = sor.user_id
                 JOIN hr_employee hem ON hem.resource_id = rre.id
-                JOIN hr_department hde ON hde.id = hem.department_id
+                LEFT JOIN hr_department hde ON hde.id = hem.department_id
                 JOIN product_product ppr ON ppr.id = sol.product_id
                 JOIN product_template pte ON pte.id = ppr.product_tmpl_id
                 JOIN product_category pca ON pca.id = pte.categ_id
@@ -26,7 +29,70 @@ class sale_order_line(osv.osv, metric_support):
            'defaults': {
                 'group_by': ['order_date_month']
            }
-        }
+        },
+        'graph_total_sales_quantity': {
+           'query': """
+                SELECT {group_sql} AS "{group_ref}", sum(product_uom_qty) as total_sales_quantity
+                FROM sale_order_line sol
+                JOIN sale_order sor ON sor.id = sol.order_id
+                JOIN res_company rco ON rco.id=sor.company_id
+                JOIN sale_shop ssh ON ssh.id=sor.shop_id
+                JOIN res_partner rpa ON rpa.id=sor.partner_id
+                JOIN resource_resource rre ON rre.user_id = sor.user_id
+                JOIN hr_employee hem ON hem.resource_id = rre.id
+                LEFT JOIN hr_department hde ON hde.id = hem.department_id
+                JOIN product_product ppr ON ppr.id = sol.product_id
+                JOIN product_template pte ON pte.id = ppr.product_tmpl_id
+                JOIN product_category pca ON pca.id = pte.categ_id
+                where TRUE {generated}
+                """,
+           'defaults': {
+                'group_by': ['order_date_month']
+           }
+        },
+        'graph_kitchen_sales': {
+           'query': """
+                SELECT {group_sql} AS "{group_ref}", sum(price_unit * product_uom_qty)/1000000000 as kitchen_sales
+                FROM sale_order_line sol
+                JOIN sale_order sor ON sor.id = sol.order_id
+                JOIN res_company rco ON rco.id=sor.company_id
+                JOIN sale_shop ssh ON ssh.id=sor.shop_id
+                JOIN res_partner rpa ON rpa.id=sor.partner_id
+                JOIN resource_resource rre ON rre.user_id = sor.user_id
+                JOIN hr_employee hem ON hem.resource_id = rre.id
+                LEFT JOIN hr_department hde ON hde.id = hem.department_id
+                JOIN product_product ppr ON ppr.id = sol.product_id
+                JOIN product_template pte ON pte.id = ppr.product_tmpl_id
+                JOIN product_category pca ON pca.id = pte.categ_id
+                where TRUE AND pca.name = 'Kitchen Modules' {generated}
+                """,
+           'defaults': {
+                'group_by': ['order_date_month']
+           }
+        },
+        'graph_sales_by_category': {
+           'query': """
+                SELECT {group_sql} AS "{group_ref}", sum(price_unit * product_uom_qty)/1000000000 as category_sales
+                FROM sale_order_line sol
+                JOIN sale_order sor ON sor.id = sol.order_id
+                JOIN res_company rco ON rco.id=sor.company_id
+                JOIN sale_shop ssh ON ssh.id=sor.shop_id
+                JOIN res_partner rpa ON rpa.id=sor.partner_id
+                JOIN resource_resource rre ON rre.user_id = sor.user_id
+                JOIN hr_employee hem ON hem.resource_id = rre.id
+                LEFT JOIN hr_department hde ON hde.id = hem.department_id
+                JOIN product_product ppr ON ppr.id = sol.product_id
+                JOIN product_template pte ON pte.id = ppr.product_tmpl_id
+                JOIN product_category pca ON pca.id = pte.categ_id
+                where TRUE {generated}
+                """,
+           'defaults': {
+                'group_by': ['product_category'],
+                'order_by': ['category_sales desc'],
+           }
+        },           
+                    
+                    
     }
 
 
